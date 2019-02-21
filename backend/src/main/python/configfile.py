b15304faa5c905101df52af2ee88c3d85d454f9a
suchiksagar <sagarksuchi@gmail.com>
2019-02-20 20:37:45 -0500

    firebase creds - read as env vars

--- backend/src/main/python/configfile.py
+++ backend/src/main/python/configfile.py
@@ -2,7 +2,19 @@ import os
 
 
 def config():
-    config = {
-        "Key": "Pair Secrets"
+    APIKEY = os.environ["FIREB_APIKEY"]
+    AUTHDOMAIN = os.environ["FIREB_AUTHDOMAIN"]
+    DATABASEURL = os.environ["FIREB_DATABASEURL"]
+    PROJECTID = os.environ["FIREB_PROJECTID"]
+    STORAGEBUCKET = os["FIREB_STORAGEBUCKET"]
+    MESSAGINGSENDERID = os["FIREB_MESSAGINGSENDERID"]
+
+    cfg = {
+        "apiKey": APIKEY if APIKEY is not None else "",
+        "authDomain": AUTHDOMAIN if AUTHDOMAIN is not None else "",
+        "databaseURL": DATABASEURL if DATABASEURL is not None else "",
+        "projectId": PROJECTID if PROJECTID is not None else "",
+        "storageBucket": STORAGEBUCKET if STORAGEBUCKET is not None else "",
+        "messagingSenderId": MESSAGINGSENDERID if MESSAGINGSENDERID is not None else ""
     }
-    return config
+    return cfg
