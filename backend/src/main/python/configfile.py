5a68f5719c7a3a07b33cbc6114f0b342ffe01dbf
suchiksagar <sagarksuchi@gmail.com>
2019-02-20 20:58:10 -0500

    firebase creds - read as env vars

--- backend/src/main/python/configfile.py
+++ backend/src/main/python/configfile.py
@@ -2,12 +2,22 @@ import os
 
 
 def config():
-    APIKEY = os.environ["FIREB_APIKEY"]
-    AUTHDOMAIN = os.environ["FIREB_AUTHDOMAIN"]
-    DATABASEURL = os.environ["FIREB_DATABASEURL"]
-    PROJECTID = os.environ["FIREB_PROJECTID"]
-    STORAGEBUCKET = os["FIREB_STORAGEBUCKET"]
-    MESSAGINGSENDERID = os["FIREB_MESSAGINGSENDERID"]
+    APIKEY = None
+    AUTHDOMAIN = None
+    DATABASEURL = None
+    PROJECTID = None
+    STORAGEBUCKET = None
+    MESSAGINGSENDERID = None
+
+    try:
+        APIKEY = os.environ["FIREB_APIKEY"]
+        AUTHDOMAIN = os.environ["FIREB_AUTHDOMAIN"]
+        DATABASEURL = os.environ["FIREB_DATABASEURL"]
+        PROJECTID = os.environ["FIREB_PROJECTID"]
+        STORAGEBUCKET = os.environ["FIREB_STORAGEBUCKET"]
+        MESSAGINGSENDERID = os.environ["FIREB_MESSAGINGSENDERID"]
+    except KeyError:
+        print("No firebase creds specified")
 
     cfg = {
         "apiKey": APIKEY if APIKEY is not None else "",
