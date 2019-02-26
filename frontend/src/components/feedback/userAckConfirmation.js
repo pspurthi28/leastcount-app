4e39adc09646e9e5f402100187e0e6b88dbac26c
Sucharith Kshirasagar <skshiras@mathworks.com>
2019-02-26 10:57:14 -0500

    removed css file

--- frontend/src/components/feedback/userAckConfirmation.js
+++ frontend/src/components/feedback/userAckConfirmation.js
@@ -5,7 +5,6 @@ import Snackbar from '@material-ui/core/Snackbar';
 import SnackbarContent from '@material-ui/core/SnackbarContent';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
-import './userAckConfirmation.css';
 
 const styles = theme => ({
     close: {
